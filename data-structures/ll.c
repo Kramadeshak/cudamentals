3cb308fe68accdd95ad6eaab0dc593c0368e9c96
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-05-27 19:11:40 +0530

    Implemented a safer alternative to init_node where we create node instead

--- data-structures/ll.c
+++ data-structures/ll.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ll{
-	struct ll *next;
+typedef struct node{
+	struct node *next;
 	int data;
-};
+}node;
 
-void init_ll(struct ll **head, int value) {
-	(*head) = malloc(sizeof(struct ll));
-	struct ll *h = (*head);
+node *create_node(int value){
+	node *n = malloc(sizeof(node));
+	if (!n){
+		fprintf(stderr, "Out of Memory\n");
+		exit(1);
+	}
+	n->data = value;
+	n->next = NULL;
+	return n;
+}
+
+void init_ll(node **head, int value) {
+	(*head) = malloc(sizeof(node));
+	node *h = (*head);
 	h->next = NULL;
 	h->data = value;
 }
 
-void add_value_ll(struct ll *head, int value) {
-	struct ll *h = head;
+void add_value_ll(node *head, int value) {
+	node *h = head;
 	while (h->next!=NULL){
 		h=h->next;
 	}
-	h->next = malloc(sizeof(struct ll));
+	h->next = malloc(sizeof(node));
 	h->next->data=value;
 }
 
-void print_ll(struct ll *h) {
+void print_ll(node *h) {
 	while (h != NULL) {
 		printf("Value: %d\n", h->data);
 		h = h->next;
@@ -30,9 +41,9 @@ void print_ll(struct ll *h) {
 }
 
 int main(){
-	printf("Size of the struct: %ld\n", sizeof(struct ll));
-	struct ll *head;
-	init_ll(&head,1);
+	printf("Size of the struct: %ld\n", sizeof(node));
+	node *head;
+	head = create_node(1);
 	add_value_ll(head, 2);
 	add_value_ll(head, 3);
 	add_value_ll(head, 4);
