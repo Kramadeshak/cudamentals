1bce770a8de566219a6ae89cb8c32b4adfa2600c
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-05-31 23:08:28 +0530

    Added create node and printing to b-tree.c

--- data-structures/b-tree.c
+++ data-structures/b-tree.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct bTree {
+typedef struct bTree {
 	struct bTree* next;
 	struct bTree* prev;
 	int value;
-};
+}bTree;
 
-void new_btree(struct bTree *b) {
-	b = malloc(sizeof(struct bTree));
+tree *create_node(int value){
+	tree *n = malloc(sizeof(bTree));
+	if (!n){
+		fprintf(stderr, "Out of Memory\n");
+		exit(1);
+	}
+	n->data = value;
+	n->left = NULL;
+	n->right = NULL;
+	return n;
 }
 
+void print_tree_inorder(tree *h) {
+	if (h->left != NULL) {
+		print_tree_inorder(h->left);
+	}
+	printf("%d", h->data);
+	if (h->right != NULL) {
+		print_tree_inorder(h->right);
+	}
+}
+
+void print_tree_preorder(tree *h) {
+	printf("%d", h->data);
+	if (h->left != NULL) {
+		print_tree_preorder(h->left);
+	}
+	if (h->right != NULL) {
+		print_tree_preorder(h->right);
+	}
+}
+
+void print_tree_postorder(tree *h) {
+	if (h->left != NULL) {
+		print_tree_postorder(h->left);
+	}
+	if (h->right != NULL) {
+		print_tree_postorder(h->right);
+	}
+	printf("%d", h->data);
+}
+
+
 int main(){
-	printf("Here is the size of a bTree-\n");
 	printf("%ld\n", sizeof(struct bTree));
 	printf("int - %ld\nchar -  %ld\nfloat -  %ld\ndouble -  %ld\n", sizeof(int), sizeof(char), sizeof(float), sizeof(double));
 }
