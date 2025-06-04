899de74013c6f090ceddd08b68174e6ff19bc341
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-06-04 22:18:03 +0530

    Added function to create node

--- data-structures/b-tree.c
+++ data-structures/b-tree.c
@@ -9,6 +9,16 @@ typedef struct BTreeNode {
   struct BTreeNode *link[MAX + 1];
 }BTreeNode;
 
+struct BTreeNode *createNode(int val, struct BTreeNode *child) {
+  struct BTreeNode *newNode;
+  newNode = (struct BTreeNode *)malloc(sizeof(struct BTreeNode));
+  newNode->val[1] = val;
+  newNode->count = 1;
+  newNode->link[0] = root;
+  newNode->link[1] = child;
+  return newNode;
+}
+
 int main() {
 	printf("Size of the struct: %ld\n", sizeof(BTreeNode));
 }
