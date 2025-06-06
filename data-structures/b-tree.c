d1648dd563aa7333e3ee3539fc81415c767447a3
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-06-06 20:45:53 +0530

    Added traversal func, root node and added function

--- data-structures/b-tree.c
+++ data-structures/b-tree.c
@@ -9,6 +9,8 @@ typedef struct BTreeNode {
   struct BTreeNode *link[MAX + 1];
 }BTreeNode;
 
+struct BTreeNode *root;
+
 struct BTreeNode *createNode(int val, struct BTreeNode *child) {
   struct BTreeNode *newNode;
   newNode = (struct BTreeNode *)malloc(sizeof(struct BTreeNode));
@@ -19,8 +21,7 @@ struct BTreeNode *createNode(int val, struct BTreeNode *child) {
   return newNode;
 }
 
-void insertNode(int val, int pos, struct BTreeNode *node,
-        struct BTreeNode *child) {
+void insertNode(int val, int pos, struct BTreeNode *node, struct BTreeNode *child) {
   int j = node->count;
   while (j > pos) {
     node->val[j + 1] = node->val[j];
@@ -32,6 +33,32 @@ void insertNode(int val, int pos, struct BTreeNode *node,
   node->count++;
 }
 
+void traversal(struct BTreeNode *myNode) {
+  int i;
+  if (myNode) {
+    for (i = 0; i < myNode->count; i++) {
+      traversal(myNode->link[i]);
+      printf("%d ", myNode->val[i + 1]);
+    }
+    traversal(myNode->link[i]);
+  }
+}
+
 int main() {
 	printf("Size of the struct: %ld\n", sizeof(BTreeNode));
+  	int val, ch;
+  	insert(8);
+  	insert(9);
+  	insert(10);
+  	insert(11);
+  	insert(15);
+  	insert(16);
+  	insert(17);
+  	insert(18);
+  	insert(20);
+  	insert(23);
+
+  	traversal(root);
+
+  	printf("\n");
 }
