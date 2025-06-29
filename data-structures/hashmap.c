bf2a527d68711f61a859d5eff4f6cd0b49902435
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-06-29 21:07:05 +0530

    Removed old hash function. trying to adapt ht_create instaed

--- data-structures/hashmap.c
+++ data-structures/hashmap.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct {
-    const char* key;  // key is NULL if this slot is empty
+    const char* key;
     void* value;
 } ht_entry;
 
-// Hash table structure: create with ht_create, free with ht_destroy.
 struct ht {
-    ht_entry* entries;  // hash slots
-    size_t capacity;    // size of _entries array
-    size_t length;      // number of items in hash table
+    ht_entry* entries;
+    size_t capacity;
+    size_t length;
 };
 
-#define INITIAL_CAPACITY 16  // must not be zero
+#define INITIAL_CAPACITY 16
 
-ht* ht_create(void) {
-    // Allocate space for hash table struct.
-    ht* table = malloc(sizeof(ht));
+struct ht* ht_create(void) {
+    struct ht* table = malloc(sizeof(struct ht));
     if (table == NULL) {
         return NULL;
     }
     table->length = 0;
     table->capacity = INITIAL_CAPACITY;
 
-    // Allocate (zero'd) space for entry buckets.
     table->entries = calloc(table->capacity, sizeof(ht_entry));
     if (table->entries == NULL) {
-        free(table); // error, free table before we return!
+        free(table);
         return NULL;
     }
     return table;
 }
+
 int main() {
-	/*printf("Size of the struct: %ld\n", sizeof(hashmap));*/
-	// starting a simple implementation first before moving to a complex one
-	int arr[10], temp;
-	// initializing array to 0
-	printf("Size of the hashmap: %ld\n", sizeof(arr));
-	for (int i=0;i<10;i++) {
-		arr[i]=0;
-	}
+	printf("Size of the struct: %ld\n", sizeof(struct ht));
+	struct ht *new_ht = ht_create();
+	int temp;
 	while (1) {
 		printf("Please enter a value: ");
 		scanf("%d", &temp);
