62adde2b773c78b1dd0e78ed50c260e03cebeefe
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-07-03 18:01:16 +0530

    Added ht_set_entries

--- data-structures/hashmap.c
+++ data-structures/hashmap.c
@@ -64,6 +64,35 @@ void* ht_get(ht* table, const char* key) {
 	return NULL;
 }
 
+const char* ht_set(ht* table, const char* key, void* value){
+	assert(value!=NULL);
+	if(value == NULL){
+		return NULL;
+	}
+	if(table->length >= table->capacity/2){
+		if(!ht_expand(table)){
+			return NULL;
+		}
+	}
+	return ht_set_entry(table->entries,table->capacity,key,value,&table->length);
+}
+
+static const char* ht_set_entry(ht_entry* entries, size_t capacity,
+        const char* key, void* value, size_t* plength) {
+    uint64_t hash = hash_key(key);
+    size_t index = (size_t)(hash & (uint64_t)(capacity - 1));
+
+    while (entries[index].key != NULL) {
+        if (strcmp(key, entries[index].key) == 0) {
+            entries[index].value = value;
+            return entries[index].key;
+        }
+        index++;
+        if (index >= capacity) {
+            index = 0;
+        }
+    }
+
 int main() {
 	printf("Size of the struct: %ld\n", sizeof(struct ht));
 	struct ht *new_ht = ht_create();
