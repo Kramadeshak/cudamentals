189a8f50311d633d45ef37f32d83154015108df4
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-07-06 21:15:45 +0530

    Resolved all bugs

--- data-structures/hashmap.c
+++ data-structures/hashmap.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <assert.h>
 typedef struct {
     const char* key;
     void* value;
@@ -17,7 +18,7 @@ typedef struct {
     const char* key;
     void* value;
 
-    ht* _table;
+    struct ht* _table;
     size_t _index;
 } hti;
 
@@ -42,7 +43,7 @@ hti ht_iterator(struct ht* table) {
 }
 
 bool ht_next(hti* it) {
-    ht* table = it->_table;
+    struct ht* table = it->_table;
     while (it->_index < table->capacity) {
         size_t i = it->_index;
         it->_index++;
@@ -128,7 +129,7 @@ static bool ht_expand(struct ht* table) {
     return true;
 }
 
-void* ht_get(ht* table, const char* key) {
+void* ht_get(struct ht* table, const char* key) {
 	uint64_t hash = hash_key(key);
 	size_t index = (size_t)(hash & (uint64_t)(table->capacity - 1));
 	while (table->entries[index].key != NULL){
@@ -143,7 +144,7 @@ void* ht_get(ht* table, const char* key) {
 	return NULL;
 }
 
-const char* ht_set(ht* table, const char* key, void* value){
+const char* ht_set(struct ht* table, const char* key, void* value){
 	assert(value!=NULL);
 	if(value == NULL){
 		return NULL;
@@ -154,23 +155,6 @@ const char* ht_set(ht* table, const char* key, void* value){
 		}
 	}
 	return ht_set_entry(table->entries,table->capacity,key,value,&table->length);
-}
-
-static const char* ht_set_entry(ht_entry* entries, size_t capacity,
-        const char* key, void* value, size_t* plength) {
-    uint64_t hash = hash_key(key);
-    size_t index = (size_t)(hash & (uint64_t)(capacity - 1));
-
-    while (entries[index].key != NULL) {
-        if (strcmp(key, entries[index].key) == 0) {
-            entries[index].value = value;
-            return entries[index].key;
-        }
-        index++;
-        if (index >= capacity) {
-            index = 0;
-        }
-    }
 }
 
 int main() {
