400b40f18a89e8b65bd425897cb2dbb7fd37e45b
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-07-06 21:22:51 +0530

    Added content to main function

--- data-structures/hashmap.c
+++ data-structures/hashmap.c
@@ -158,6 +158,34 @@ const char* ht_set(struct ht* table, const char* key, void* value){
 }
 
 int main() {
-	printf("Size of the struct: %ld\n", sizeof(struct ht));
-	struct ht *new_ht = ht_create();
+    printf("Size of the struct: %ld\n", sizeof(struct ht));
+    ht* counts = ht_create();
+    if (counts == NULL) {
+        exit_nomem();
+    }
+    char word[101];
+    while (scanf("%100s", word) != EOF) {
+        void* value = ht_get(counts, word);
+        if (value != NULL) {
+            int* pcount = (int*)value;
+            (*pcount)++;
+            continue;
+        }
+        int* pcount = malloc(sizeof(int));
+        if (pcount == NULL) {
+            exit_nomem();
+        }
+        *pcount = 1;
+        if (ht_set(counts, word, pcount) == NULL) {
+            exit_nomem();
+        }
+    }
+    hti it = ht_iterator(counts);
+    while (ht_next(&it)) {
+        printf("%s %d\n", it.key, *(int*)it.value);
+        free(it.value);
+    }
+    printf("%d\n", (int)ht_length(counts));
+    ht_destroy(counts);
+    return 0;struct ht *new_ht = ht_create();
 }
